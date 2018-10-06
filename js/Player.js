74b291475aeb1e53cfbc34526147bf3354b49afd
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2018-10-06 14:25:30 +0200

    add token creator function on Player

--- js/Player.js
+++ js/Player.js
@@ -4,6 +4,22 @@ class Player {
         this.id = id;
         this.color = color;
         this.active = active;
-        this.token = [];
+        this.tokens = this.createTokens(21);
+    }
+
+    /**
+     * Creates token objects for player
+     * @param   {Number}   num - Number of token objects to be created
+     * @return  {array}     tokens - an array of new token objects
+     */
+    createTokens(num) {
+        let tokens = [];
+
+        for (let i = 0; i < num; i += 1) {
+            const token = new Token(this, i);
+            tokenArray.push(token);
+        }
+
+        return tokens;
     }
 }
