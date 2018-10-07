597b59105585bba134cc53beae98c3faa86bfa55
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2018-10-07 09:33:32 +0200

    Build up game class - deviated from course

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,38 @@
-class Game {};
+class Game {
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers(2);
+        this.ready = false;
+    }
+
+    randomColor() {
+        return Math.floor( Math.random() * 255 );
+    }
+
+    createColor() {
+        return `rgb(${this.randomColor()},  ${this.randomColor()}, ${this.randomColor()})`;
+    }
+
+    /**
+     * Creates two player objects
+     * @return  {Array}    An array of two Player objects.
+     */
+
+    createPlayers(num) {
+        // let players = [
+        //     new Player('Player 1', 1, '#e1525', true),
+        //     new Player('Player 2', 2, '#e59a13')
+        // ];
+
+        let players = [];
+
+        for (let i = 0; i < num; i += 1 ) {
+            const player = new Player(`Player-${i}`, i, this.createColor());
+            players.push(player);
+        }
+
+        return players;
+    }
+
+    startGame() {}
+}
