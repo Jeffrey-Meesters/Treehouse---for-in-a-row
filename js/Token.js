6c40b4a1059991a9acc4258ead93830e0762da25
Jeffrey Meesters <jjeffreymeesters@gmail.com>
2018-10-07 20:35:44 +0200

    add token movement

--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,7 @@ class Token {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.droped = false;
+        this.columnLocation = 0;
     }
 
     /**
@@ -22,5 +23,31 @@ class Token {
         token.setAttribute('id', this.id);
         token.setAttribute('class', 'token');
         token.style.backgroundColor = this.owner.color;
+    }
+
+    get offsetLeft() {
+        return this.htmlToken.offsetLeft;
+    }
+
+    moveLeft() {
+        if (this.columnLocation > 0) {
+            this.htmlToken.style.left = this.offsetLeft - 76;
+            this.columnLocation -= 1;
+        }
+    }
+
+    moveRight(columns = 7) {
+        if (this.columnLocation !== columns - 1) { // < columns -1
+            this.htmlToken.style.left = this.offsetLeft + 76;
+            this.columnLocation += 1;
+        }
+    }
+
+    drop(target, reset) {
+        this.droped = true;
+
+        $(this.htmlToken).animate({
+            top: (target.y * target.diameter)
+        }, 750, 'easeOutBounce', reset);
     }
 }
